a7d4d270d54cd598e05e6f3bb312cc3daa3aa5c6
ConMcEvoy <mcevoyco@gmail.com>
2015-07-05 17:21:08 +0100

    commit the final code :) twitters working.

--- TwitterSearcher/TwitterSearcher/MainPage.xaml.cs
+++ TwitterSearcher/TwitterSearcher/MainPage.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Linq;
+using TwitterSearcher.Model;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -26,5 +31,49 @@ namespace TwitterSearcher
         {
             this.InitializeComponent();
         }
+
+        private ObservableCollection<MyTweet> _tweets = new ObservableCollection<MyTweet>();
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            TweetList.ItemsSource = _tweets;
+            LoadTweets();
+        }
+
+  
+
+        private async void LoadTweets()
+        {
+            Uri twitterApiQuery = new Uri("http://search.twitter.com/search.atom?q=win10+OR+windows10");
+            var client = new HttpClient();
+            var response = await client.GetAsync(twitterApiQuery);
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            Debug.WriteLine(content);
+
+            var doc = XDocument.Parse(content);
+            XNamespace ns = "http://www.w3.org/2005/Atom";
+
+            var items = from item in doc.Descendants(ns + "entry")
+                        select new MyTweet()
+                        {
+                            Message = item.Element(ns + "title").Value,
+                            Image =
+                                (from XElement xe in item.Descendants(ns + "link")
+                                 where xe.Attribute("type").Value == "image/png"
+                                 select xe.Attribute("href").Value).First()
+                        };
+
+            _tweets.Clear();
+
+            foreach(MyTweet t in items)
+            {
+                _tweets.Add(t);
+            }
+
+         }
+
+
     }
 }
---
+++ TwitterSearcher/TwitterSearcher/Model/MyTweet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterSearcher.Model
+{
+    class MyTweet
+    {
+        public String Message { get; set; }
+        public String Image { get; set; }
+    }
+}
